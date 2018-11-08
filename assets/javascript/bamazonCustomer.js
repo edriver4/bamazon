6e6da729abb1e67dcf435985b6cab4bfe0f2a1ca
Bradley <brad.driver33@gmail.com>
2018-11-08 18:36:36 -0500

    Fixed updateData function where it will update the database without causing an error

--- assets/javascript/bamazonCustomer.js
+++ assets/javascript/bamazonCustomer.js
@@ -19,6 +19,8 @@ connection.connect((err) => {
     retrieveAllItems();
 });
 
+// This Function is retrieves all the data from the database and displaying it as a table within the terminal.
+// This function returns the response with all the information within the database. 
 function retrieveAllItems() {
     connection.query('SELECT * FROM products', (err, res) => {
         if (err) throw err;
@@ -27,7 +29,10 @@ function retrieveAllItems() {
     })
 }
 
-// Check for the store for the quantity of a particular item
+// Check for the store for the quantity of a particular item.
+// Prompts the user to input the item id of the item they want.
+// Then within the promise (.then) it returns the item that the customer selected.
+// Then prompts the user how much they would like to purchase.
 function propmtCustItem(inventory) {
     inquirer
         .prompt({
@@ -41,12 +46,13 @@ function propmtCustItem(inventory) {
             const choiceId = parseInt(val.choice)
             console.log('This is the choiceId ===>', choiceId);
             var product = checkItem(choiceId, inventory)
-            console.log('Product I slected ===>', product)
+            console.log('Product I selected ===>', product)
 
             promptCustomerForQuantity(product)
         })
 }
-
+// This function checks to see if the item id that was entered is in the database. 
+// If the item is it returns everything about that item.
 function checkItem(userChoice, inventory) {
     for (var i = 0; i < inventory.length; i++) {
         if (inventory[i].item_id === userChoice) {
@@ -72,13 +78,14 @@ function promptCustomerForQuantity(product) {
             const quantitySelected = parseInt(val.quantity);
             console.log('This is the quantity that the customer selected ===>', quantitySelected);
             checkQuantity(quantitySelected, product);
+            updateData(product, quantitySelected);
         })
 }
 
 function checkQuantity(selectedAmount, product) {
     if (selectedAmount <= product.stock_quantity) {
         console.log("Order accepted.");
-    } else if (product.stock_quantity === 0){
+    } else if (product.stock_quantity === 0) {
         console.log(`'Sorry, we don\'t have anymore of ${product.product_name}'`);
     }
     else {
@@ -86,3 +93,16 @@ function checkQuantity(selectedAmount, product) {
     }
 
 }
+
+// Function to update the data within the database. Updating the quantity when the item is ordered.
+function updateData(product, quantitySelected) {
+    connection.query(`UPDATE products SET stock_quantity = ${product.stock_quantity - quantitySelected} WHERE item_id = ${product.item_id}`, (err, res) => {
+        if (err) throw err;
+        console.log('=================================');
+        console.log('Your total is: $' + (product.price * quantitySelected));
+        console.log('=================================');
+        // console.log(`${res.affectedRows} row(s) was affected.`);
+        retrieveAllItems();
+    })
+}
+
